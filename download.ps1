1e4c105d5ae7f61adebd3cb7fbd871bcbdf50dff
theurbanpenguin <andrew.mallett@theurbanpenguin.com>
2026-09-03 12:55:12 +0100

    Add files via upload

---
+++ download.ps1
@@ -0,0 +1,6 @@
+﻿$MyPath = $env:PSModulePath -split ';'
+$MyModule = (Join-Path $MyPath[0] "MyTools")
+New-Item -Path $MyModule  -ItemType Directory -Force
+Invoke-WebRequest `
+    -URI "https://raw.githubusercontent.com/theurbanpenguin/pshell/main/MyTools.psm1" `
+    -OutFile (Join-Path $MyModule "MyTools.psm1")
